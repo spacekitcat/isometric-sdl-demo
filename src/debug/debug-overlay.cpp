1f57b1d004628729a0087422b30c65422d566de2
Lisa Burton <spacekitcat@protonmail.com>
2021-08-29 15:00:34 +0100

    fix: memory leak.

--- src/debug/debug-overlay.cpp
+++ src/debug/debug-overlay.cpp
@@ -25,19 +25,23 @@ void DebugOverlay::render() {
 }
 
 void DebugOverlay::renderText(std::string text) {
+  // Black outline (background)
   TTF_SetFontOutline(_font, 1);
   SDL_Color textBgColor = {0, 0, 0};
   SDL_Surface *text_surface_bg =
       TTF_RenderText_Blended(_font, text.c_str(), textBgColor);
 
+  // White fill (foreground)
   TTF_SetFontOutline(_font, 0);
   SDL_Color textFgColor = {255, 255, 255};
   SDL_Surface *text_surface_fg =
       TTF_RenderText_Blended(_font, text.c_str(), textFgColor);
 
+  // Offset to center
   SDL_Rect offset = {.x = 1, .y = 1};
   SDL_BlitSurface(text_surface_fg, NULL, text_surface_bg, &offset);
 
+  // Text conversion
   SDL_Texture *tex =
       SDL_CreateTextureFromSurface(_sdlManager->getRenderer(), text_surface_bg);
 
@@ -45,7 +49,7 @@ void DebugOverlay::renderText(std::string text) {
       .x = 100, .y = 25, .w = text_surface_bg->w, .h = text_surface_bg->h};
   SDL_RenderCopy(_sdlManager->getRenderer(), tex, NULL, &dest);
 
-  // SDL_DestroyTexture(tex);
+  SDL_DestroyTexture(tex);
 
   SDL_FreeSurface(text_surface_bg);
   SDL_FreeSurface(text_surface_fg);
