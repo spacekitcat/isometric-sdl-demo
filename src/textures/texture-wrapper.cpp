dbfedfb6fd80ff87ae9450fd15a1a68659c2ccbe
Lisa Burton <spacekitcat@protonmail.com>
2021-09-02 19:36:32 +0100

    feat: add texture-wrappe class

---
+++ src/textures/texture-wrapper.cpp
@@ -0,0 +1,11 @@
+#include "texture-wrapper.hpp"  
+	
+TextureWrapper::TextureWrapper()
+{
+	
+}
+	
+TextureWrapper::~TextureWrapper()
+{
+	
+}
---
+++ src/textures/texture-wrapper.hpp
@@ -0,0 +1,16 @@
+#ifndef TEXTUREWRAPPER_H
+#define TEXTUREWRAPPER_H
+
+#include <SDL.h>
+
+class TextureWrapper  
+{
+	private:
+
+	public:
+
+		TextureWrapper();
+		~TextureWrapper();
+
+};
+#endif
