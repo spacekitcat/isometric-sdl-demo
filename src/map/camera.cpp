aedba83df03a5d6aee3e2ef3a8cd7ed74960e5ea
Lisa Burton <spacekitcat@protonmail.com>
2021-09-06 15:39:43 +0100

    refactor: update game logic according to the number of elapsed ticks between updates. decoupled player position concept from the camera. refactored unit testing

--- src/map/camera.cpp
+++ src/map/camera.cpp
@@ -1,8 +1,18 @@
 #include "camera.hpp"
 #include "../util/pair-operators.hpp"
 
+Camera::Camera() { _targetPlayer = NULL; }
+
+Camera::~Camera() {}
+
 std::pair<float, float> Camera::getPosition() { return _position; }
 
-void Camera::applyDelta(std::pair<float, float> delta) {
-  _position = PairOperators::addPair(_position, delta);
+void Camera::setPosition(std::pair<float, float> position) {}
+
+void Camera::setTarget(Player *player) { _targetPlayer = player; }
+
+void Camera::update(long int ticks) {
+  if (_targetPlayer != NULL) {
+    _position = _targetPlayer->getPosition();
+  }
 }
