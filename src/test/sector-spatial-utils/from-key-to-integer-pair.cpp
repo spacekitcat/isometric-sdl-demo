dd5d89693f81f4ee03b6ad4441df46f2501cf992
Lisa Burton <spacekitcat@protonmail.com>
2021-09-11 16:10:26 +0100

    feat: more unit tests for fromKeyToIntegerPair

--- src/test/sector-spatial-utils/from-key-to-integer-pair.cpp
+++ src/test/sector-spatial-utils/from-key-to-integer-pair.cpp
@@ -20,3 +20,86 @@ TEST_CASE("Correctly converts 0,-1 (string) to integer pair",
   REQUIRE(result.first == 0);
   REQUIRE(result.second == -1);
 }
+
+TEST_CASE("Correctly converts 0,1 (string) to integer pair",
+          "[Map::SectorSpatialUtils::fromKeyToIntegerPair]") {
+  auto sut = new SectorSpatialUtils();
+
+  auto result = sut->fromKeyToIntegerPair("0,1");
+  REQUIRE(result.first == 0);
+  REQUIRE(result.second == 1);
+}
+
+TEST_CASE("Correctly converts -1,0 (string) to integer pair",
+          "[Map::SectorSpatialUtils::fromKeyToIntegerPair]") {
+  auto sut = new SectorSpatialUtils();
+
+  auto result = sut->fromKeyToIntegerPair("-1,0");
+  REQUIRE(result.first == -1);
+  REQUIRE(result.second == 0);
+}
+
+TEST_CASE("Correctly converts 1,0 (string) to integer pair",
+          "[Map::SectorSpatialUtils::fromKeyToIntegerPair]") {
+  auto sut = new SectorSpatialUtils();
+
+  auto result = sut->fromKeyToIntegerPair("1,0");
+  REQUIRE(result.first == 1);
+  REQUIRE(result.second == 0);
+}
+
+TEST_CASE("Correctly converts 100,100 (string) to integer pair",
+          "[Map::SectorSpatialUtils::fromKeyToIntegerPair]") {
+  auto sut = new SectorSpatialUtils();
+
+  auto result = sut->fromKeyToIntegerPair("100,100");
+  REQUIRE(result.first == 100);
+  REQUIRE(result.second == 100);
+}
+
+TEST_CASE("Correctly converts -100,-100 (string) to integer pair",
+          "[Map::SectorSpatialUtils::fromKeyToIntegerPair]") {
+  auto sut = new SectorSpatialUtils();
+
+  auto result = sut->fromKeyToIntegerPair("-100,-100");
+  REQUIRE(result.first == -100);
+  REQUIRE(result.second == -100);
+}
+
+TEST_CASE(
+    "Correctly converts integer pair with extra space after the delimiter",
+    "[Map::SectorSpatialUtils::fromKeyToIntegerPair]") {
+  auto sut = new SectorSpatialUtils();
+
+  auto result = sut->fromKeyToIntegerPair("100, 100");
+  REQUIRE(result.first == 100);
+  REQUIRE(result.second == 100);
+}
+
+TEST_CASE(
+    "Correctly converts integer pair with extra space before the delimiter",
+    "[Map::SectorSpatialUtils::fromKeyToIntegerPair]") {
+  auto sut = new SectorSpatialUtils();
+
+  auto result = sut->fromKeyToIntegerPair("100 ,100");
+  REQUIRE(result.first == 100);
+  REQUIRE(result.second == 100);
+}
+
+TEST_CASE("Correctly converts integer pair with extra space prefix",
+          "[Map::SectorSpatialUtils::fromKeyToIntegerPair]") {
+  auto sut = new SectorSpatialUtils();
+
+  auto result = sut->fromKeyToIntegerPair(" 100,100");
+  REQUIRE(result.first == 100);
+  REQUIRE(result.second == 100);
+}
+
+TEST_CASE("Correctly converts integer pair with extra space suffix",
+          "[Map::SectorSpatialUtils::fromKeyToIntegerPair]") {
+  auto sut = new SectorSpatialUtils();
+
+  auto result = sut->fromKeyToIntegerPair("100,100 ");
+  REQUIRE(result.first == 100);
+  REQUIRE(result.second == 100);
+}
