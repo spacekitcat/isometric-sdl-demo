ff7e0a0b3c4842c60256026fe90cc3fe65f84894
Lisa Burton <spacekitcat@protonmail.com>
2021-09-18 14:23:28 +0100

    feat: skeleton for screen coordinate mapper tests. boolean toggle for enabling/disabling music

--- src/test/screen-coordinate-mapper/center-in-screen-space.cpp
+++ src/test/screen-coordinate-mapper/center-in-screen-space.cpp
@@ -5,4 +5,7 @@
 #include "../../map/screen-coordinate-mapper.hpp"
 
 TEST_CASE("Maps world 0,0 to screen index 50%,50%",
-          "[Map::screen-coordinate-mapper::centerInScreenSpace]") {}
+          "[Map::screen-coordinate-mapper::centerInScreenSpace]") {
+
+  // auto sut = new ScreenCoordinateMapper();
+}
