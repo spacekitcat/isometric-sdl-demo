60c4a17155f6bba557ba0322821c0b77ed915971
Lisa Burton <8135257+spacekitcat@users.noreply.github.com>
2021-09-18 15:01:12 +0100

    refactor: camera doesn't need to know about SDLManager (#25)

--- src/map/camera.hpp
+++ src/map/camera.hpp
@@ -4,18 +4,16 @@
 
 #include "../map/camera-target.hpp"
 #include "../map/camera.hpp"
-#include "../render/sdl-manager.hpp"
 #include "../util/pair-operators.hpp"
 
 class Camera {
 private:
-  std::shared_ptr<SDLManager> _sdlManager;
   std::pair<float, float> _position;
   float _zoom;
   CameraTarget *_targetPlayer;
 
 public:
-  Camera(std::shared_ptr<SDLManager> sdlManager);
+  Camera();
 
   ~Camera();
 
--- src/test/screen-coordinate-mapper/center-in-screen-space.cpp
+++ src/test/screen-coordinate-mapper/center-in-screen-space.cpp
@@ -2,10 +2,17 @@
 #include <iostream>
 
 #include "../../config/configuration.hpp"
+#include "../../map/camera.hpp"
 #include "../../map/screen-coordinate-mapper.hpp"
 
 TEST_CASE("Maps world 0,0 to screen index 50%,50%",
           "[Map::screen-coordinate-mapper::centerInScreenSpace]") {
 
-  // auto sut = new ScreenCoordinateMapper();
+  auto specifiedConfiguration = std::make_shared<Configuration>();
+  auto specifiedCamera = std::make_shared<Camera>();
+
+  auto sut =
+      new ScreenCoordinateMapper(specifiedConfiguration, specifiedCamera);
+
+  sut.centerInScreenSpace(std::make_pair(0, 0));
 }
