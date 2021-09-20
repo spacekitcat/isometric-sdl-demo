a92607522cdc695dab8b014f9bd66eba93c5c47b
Lisa Burton <spacekitcat@protonmail.com>
2021-09-20 21:43:21 +0100

    refactor: more complete center-in-screen-space unit test suite

--- src/test/screen-coordinate-mapper/center-in-screen-space.cpp
+++ src/test/screen-coordinate-mapper/center-in-screen-space.cpp
@@ -12,7 +12,7 @@ TEST_CASE("Maps world 0,0 (1x) to screen index 50%,50% (1x)",
   auto specifiedCamera = std::make_shared<Camera>();
 
   specifiedConfiguration->setWindowDimensions(std::make_pair(600, 600));
-  specifiedCamera->setZoom(1.0f);
+  specifiedCamera->setZoom(1.0F);
 
   auto *sut =
       new ScreenCoordinateMapper(specifiedConfiguration, specifiedCamera);
@@ -22,14 +22,31 @@ TEST_CASE("Maps world 0,0 (1x) to screen index 50%,50% (1x)",
   REQUIRE(result.second == 300);
 }
 
-TEST_CASE("Maps world 0,0 (.5x) to screen index 50%,50% (.5x)",
+TEST_CASE("Maps world 0,0 (.75x) to screen index 50%,50%",
           "[Map::screen-coordinate-mapper::centerInScreenSpace]") {
 
   auto specifiedConfiguration = std::make_shared<Configuration>();
   auto specifiedCamera = std::make_shared<Camera>();
 
   specifiedConfiguration->setWindowDimensions(std::make_pair(600, 600));
-  specifiedCamera->setZoom(.5f);
+  specifiedCamera->setZoom(0.75F);
+
+  auto *sut =
+      new ScreenCoordinateMapper(specifiedConfiguration, specifiedCamera);
+
+  auto result = sut->centerInScreenSpace(std::make_pair(0, 0));
+  REQUIRE(result.first == 400);
+  REQUIRE(result.second == 400);
+}
+
+TEST_CASE("Maps world 0,0 (.5x) to screen index",
+          "[Map::screen-coordinate-mapper::centerInScreenSpace]") {
+
+  auto specifiedConfiguration = std::make_shared<Configuration>();
+  auto specifiedCamera = std::make_shared<Camera>();
+
+  specifiedConfiguration->setWindowDimensions(std::make_pair(600, 600));
+  specifiedCamera->setZoom(0.5F);
 
   auto *sut =
       new ScreenCoordinateMapper(specifiedConfiguration, specifiedCamera);
@@ -37,4 +54,21 @@ TEST_CASE("Maps world 0,0 (.5x) to screen index 50%,50% (.5x)",
   auto result = sut->centerInScreenSpace(std::make_pair(0, 0));
   REQUIRE(result.first == 600);
   REQUIRE(result.second == 600);
+}
+
+TEST_CASE("Maps world 0,0 (.25x) to screen index",
+          "[Map::screen-coordinate-mapper::centerInScreenSpace]") {
+
+  auto specifiedConfiguration = std::make_shared<Configuration>();
+  auto specifiedCamera = std::make_shared<Camera>();
+
+  specifiedConfiguration->setWindowDimensions(std::make_pair(600, 600));
+  specifiedCamera->setZoom(0.25F);
+
+  auto *sut =
+      new ScreenCoordinateMapper(specifiedConfiguration, specifiedCamera);
+
+  auto result = sut->centerInScreenSpace(std::make_pair(0, 0));
+  REQUIRE(result.first == 1200);
+  REQUIRE(result.second == 1200);
 }
